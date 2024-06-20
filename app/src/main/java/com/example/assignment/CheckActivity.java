65f55044c813f5a3af4749098039e7bb3472468e
GiorgiGogochishvili <135644744+GiorgiGogochishvili@users.noreply.github.com>
2024-06-20 10:57:56 +0400

    first

---
+++ app/src/main/java/com/example/assignment/CheckActivity.java
@@ -0,0 +1,42 @@
+package com.example.assignment;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class CheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_home) {
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            } else if (item.getItemId() == R.id.nav_search) {
+                startActivity(new Intent(this, SearchActivity.class));
+                return true;
+            }  else if (item.getItemId() == R.id.nav_settings) {
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            } else if (item.getItemId() == R.id.nav_check) {
+                startActivity(new Intent(this, CheckActivity.class));
+                return true;
+            } else if (item.getItemId() == R.id.nav_exit) {
+                startActivity(new Intent(this, ExitActivity.class));
+                return true;
+            }
+            return false;
+        });
+    }
+}
